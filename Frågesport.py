8ef3012cb4b463a558a91e512f5b2934d75dd876
Yassin Bougmza <yassin.bougmza@elev.ga.ntig.se>
2022-09-01 13:17:21 +0200

    Lektion 2

--- Frågesport.py
+++ Frågesport.py
@@ -1,4 +1,7 @@
 
+from tkinter import N
+
+
 score=0
 name = input("Hej, vad heter du?  ")
 frågesport= (input(f"Hej {name}! Är du redo för frågesport?  "))
@@ -10,7 +13,7 @@ else:
 
 
 
-Fråga1=input("Hur många ränder finns på USAs flagga?")
+Fråga1=input("Hur många ränder finns på USAs flagga? ") 
 
 if Fråga1 == "13":
     score=score + 1
@@ -18,7 +21,7 @@ if Fråga1 == "13":
 
 else:
     score=score - 1
-    print("Tyvärr hade du fel. USAs flagga har 13 ränder. ")
+    print(f"Tyvärr hade du fel {name}. USAs flagga har 13 ränder.  ")
 
 
 Fråga2=input("Hur gammal är Hampus Eriksson? ")
@@ -27,7 +30,7 @@ if Fråga2 == "28 år" or Fråga2 == "28":
     print("Grattis du har rätt! ")
     score=score + 1
 else:
-    print("Tyvärr hade du fel. Hampus ålder är 28 år. ")
+    print(f"Tyvärr hade du fel {name}. Hampus ålder är 28 år.  ")
     score=score - 1
 
 Fråga3=input("Hur många tidszoner finns i Ryssland? ")
@@ -35,22 +38,21 @@ Fråga3=input("Hur många tidszoner finns i Ryssland? ")
 if Fråga3 == "11":
     print("Grattis du hade rätt! ")
     score=score + 1
-
 else:
-    print("Tyvärr hade du fel. Svaret var 11. ")
+    print(f"Tyvärr hade du fel {name}. Svaret var 11. ")
     score=score - 1
 
-Fråga4=input("Vad hette den turkiska staden Istanbul fram till 1923?")
+Fråga4=input("Vad hette den turkiska staden Istanbul fram till 1923? ")
 
 if Fråga4 == "Konstantinopel" or Fråga4 == "konstantinopel":
     print("Grattis du har rätt!")
     score= score + 1
 else:
-    print("Tyvärr hade du fel. Svaret var Konstantinopel.")
+    print(f"Tyvärr hade du fel {name}. Svaret var Konstantinopel. ")
     score=score -1
 
 input("Tryck på enter för att se din score")
 
-print(f"du slutade med {score}/4 poäng. ")
+print(f"du slutade med {score}/4 poäng.  ")
 
     
