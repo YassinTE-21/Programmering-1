ab4770c15d39e43b9788bf00823b08fd51808bad
Yassin Bougmza <yassin.bougmza@elev.ga.ntig.se>
2022-09-20 13:47:49 +0200

    Lektion 10

---
+++ Prime numbers.py
@@ -0,0 +1,9 @@
+num=int(input("vilket nummer vill du kolla? "))
+
+for i in range (2, num):
+    if (num % i) == 0:
+        print("Det är inte ett prim nummer ")
+        break
+else:
+    print("Det är ett prim nummer ")
+            
