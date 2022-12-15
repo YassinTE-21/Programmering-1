cbede0141198d0f28f9d907ac603edb7b5117579
Yassin Bougmza <yassin.bougmza@elev.ga.ntig.se>
2022-12-15 21:56:40 +0100

    Projektplan 1

--- Funktioner uppgift.py
+++ Funktioner uppgift.py
@@ -3,49 +3,63 @@
 
 
 def best(name):
-    name="Yassin"
-    print(f"{name} är bäst" ) 
+    name = "Yassin"
+    print(f"{name} är bäst")
+
+
 best("Yassin")
 
-print("------------------------------------------------------------------------------------")
+print(
+    "------------------------------------------------------------------------------------"
+)
+
 
 def square(number):
     # TODO Returnera talet kvadrerat
     # Ex: 5 in - 25 ut
     pass
     return number**2
+
+
 print(square(3))
 print(square(7))
 print(square(13))
 print(square(41))
 
 
-print("------------------------------------------------------------------------------------")
+print(
+    "------------------------------------------------------------------------------------"
+)
+
 
 def sums(a, b):
     # TODO Returnera summan av a och b
     # Ex: 2, 6 in - 8 ut
-    return a+b
-print(sums(3,8))
-print(sums(4,5))
-print(sums(9,12))
-print(sums(12,32))
+    return a + b
 
-print("------------------------------------------------------------------------------------")
+
+print(sums(3, 8))
+print(sums(4, 5))
+print(sums(9, 12))
+print(sums(12, 32))
+
+print(
+    "------------------------------------------------------------------------------------"
+)
 # Nu blir det lite svårare
 
 
 def maximum(a, b, c):
     # TODO Returnera det största av a,b,c
     # Ex: 3, 6, 12 in - 12 ut
-    maximum("4","7","19")
-    if a>b and a>c:
+    maximum("4", "7", "19")
+    if a > b and a > c:
         return a
-    elif b>c:
+    elif b > c:
         return b
     else:
         return c
-    
+
 
 def palindrom(ord):
     # TODO Returnera True om ord är ett palindrom
@@ -55,12 +69,14 @@ def palindrom(ord):
 
 
 def prime(nr):
-    nr=int(3)
-    for i in range (2, nr):
+    nr = int(3)
+    for i in range(2, nr):
         if (nr % i) == 0:
             print("Det är inte ett prim nummer ")
         break
     else:
         print("Det är ett prim nummer ")
-    return nr 
-print(prime(5))          
+    return nr
+
+
+print(prime(5))
---
+++ Funktioner.py
@@ -0,0 +1,12 @@
+def f(x):
+    y = x * 3 + 70
+    return y
+
+
+f15 = f(15)
+print(f15)
+print(f(127))
+
+
+def check_lista(stuff):
+    pass
